b0db991e5e07c2e208bf8eba7ecdaafee4c85307
PabloSilvaAdones <pablo.silva.adones@gmail.com>
2021-06-09 18:58:15 -0400

    .

--- entornoEvaluacion3/evaluacionDjango/hamburgame/views.py
+++ entornoEvaluacion3/evaluacionDjango/hamburgame/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Producto, Tipo
-from .forms import ProductoForm, TipoForm
+from .models import Producto, Categoria
+from .forms import ProductoForm, CategoriaForm
 # Create your views here.
 def inicio(request):
     return render(request, 'index.html')
@@ -9,7 +9,50 @@ def normales(request):
     return render(request, 'normales.html')    
 
 def premium(request):
-    return render(request, 'premium.html')       
+    return render(request, 'premium.html')   
+
+        
+def listadoProducto(request):
+# obtiene todos los datos de la tabla producto
+    productos = Producto.objects.all()
+
+    if request.method == 'POST':
+        buscar = request.POST['txtBuscar']
+        productos = Producto.objects.filter(descripcion__contains = buscar).order_by('descripcion') # select * from Categoria where nombre like '%patron%'
+# crear el contexto para ser enviado a la plantilla (render)
+    contexto = { 'productos' : productos  }
+# renderizar (unir los datos con la plantilla)
+    return render(request, 'listadoProducto.html', contexto)
+    
+def formProducto(request):
+    contexto = {'form' : ProductoForm }
+
+## guardar en la base de datos --> modelos    
+    if request.method == 'POST':
+        formulario = ProductoForm(request.POST)
+        formulario.save()
+        contexto['mensaje'] = "Los datos fueron guardados"
+        
+    return render(request, 'formProducto.html', contexto)
+
+
+def formModProducto(request, id):
+    producto = Producto.objects.get(id = id)
+    contexto = {'form' : ProductoForm(instance=producto) }
+
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, instance=producto)
+        formulario.save()
+        contexto['mensaje'] = "Los datos fueron guardados"
+        contexto['form'] = ProductoForm(instance=Producto.objects.get(id = id))
+
+    return render(request, 'formModProducto.html', contexto)
+
+def eliminarProducto(request, id):
+    producto = Producto.objects.get(id = id)
+# elimina el elemento de la base de datos
+    producto.delete()
+    return redirect(to="listadoProducto")
 
 
 
@@ -18,6 +61,56 @@ def premium(request):
 
 
 
+
+
+
+
+
+
+def listarCategoria(request):
+# obtiene todos los datos de la tabla Categoria
+    categorias = Categoria.objects.all() # select * from Categoria
+#Ejercicios: aplicar filtro por descripción en Producto
+    if request.method == 'POST':
+        buscar = request.POST['txtBuscar'] ## startswith endswith
+        categorias = Categoria.objects.filter(nombre__contains = buscar).order_by('activo', 'nombre') # select * from Categoria where nombre like '%patron%'
+
+# crear el contexto para ser enviado a la plantilla (render)
+    contexto = { 'categorias' : categorias  }
+# renderizar (unir los datos con la plantilla)
+    return render(request, 'listarCategoria.html', contexto)
+    
+
+
+def crearCategoria(request):
+    contexto = {'formulario' : CategoriaForm }
+
+## guardar en la base de datos --> modelos    
+    if request.method == 'POST':
+        formulario = CategoriaForm(request.POST)
+        formulario.save()
+        contexto['mensaje'] = "Los datos fueron guardados"
+        
+    return render(request, 'crearCategoria.html', contexto)
+
+def modificarCategoria(request, id):
+    categoria = Categoria.objects.get(id = id)
+    contexto = {'form' : CategoriaForm(instance=categoria) }
+
+    if request.method == 'POST':
+        formulario = CategoriaForm(data=request.POST, instance=categoria)
+        formulario.save()
+        contexto['mensaje'] = "Los datos fueron guardados"
+        contexto['form'] = CategoriaForm(instance=Categoria.objects.get(id = id))
+
+    return render(request, 'modificarCategoria.html', contexto)
+
+
+def eliminarCategoria(request, id):
+    categoria = Categoria.objects.get(id = id)
+# elimina el elemento de la base de datos
+    categoria.delete()
+    return redirect(to="listarCategoria")
 
 
 
