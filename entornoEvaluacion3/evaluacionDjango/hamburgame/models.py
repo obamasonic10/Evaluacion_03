032ae2d4a69c7e8c3c6145d47a48f03cb3ad94f1
obamasonic10 <victor.verdugo.buzeta@gmail.com>
2021-06-08 21:35:36 -0400

    .

--- entornoEvaluacion3/evaluacionDjango/hamburgame/models.py
+++ entornoEvaluacion3/evaluacionDjango/hamburgame/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Producto (models.Model):
+    id = models.IntegerField(primary_key = True)
+    codigoBarra = models.IntegerField(verbose_name="Código de Barras")
+    descripcion = models.CharField(max_length=50, verbose_name="Descripción")
+    precioCosto = models.IntegerField(verbose_name="Precio Costo")
+    precioVenta = models.IntegerField(verbose_name="Precio Venta")
+    tipo = models.IntegerField(verbose_name="Tipo")
+    activo = models.BooleanField(verbose_name="Activo")
