d555e9b97b4a923114da0ca8af6cd3699364a6d2
obamasonic10 <victor.verdugo.buzeta@gmail.com>
2021-06-08 21:47:06 -0400

    Update models.py

--- entornoEvaluacion3/evaluacionDjango/hamburgame/models.py
+++ entornoEvaluacion3/evaluacionDjango/hamburgame/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id = models.IntegerField(primary_key = True)
+    nombre = models.CharField(max_length=50)
+    activo = models.BooleanField()
+
+    def __str__(self):
+    
+        return self.nombre
+
 class Producto (models.Model):
     id = models.IntegerField(primary_key = True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name="Categoría")
     codigoBarra = models.IntegerField(verbose_name="Código de Barras")
     descripcion = models.CharField(max_length=50, verbose_name="Descripción")
     precioCosto = models.IntegerField(verbose_name="Precio Costo")
     precioVenta = models.IntegerField(verbose_name="Precio Venta")
-    tipo = models.IntegerField(verbose_name="Tipo")
     activo = models.BooleanField(verbose_name="Activo")
+    
+
+    def __str__(self):
+        return self.descripcion
