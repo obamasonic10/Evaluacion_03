98c3fbba4b0a4597a86b637b6529866cfcecc310
obamasonic10 <victor.verdugo.buzeta@gmail.com>
2021-06-09 10:43:35 -0400

    .

--- entornoEvaluacion3/evaluacionDjango/hamburgame/models.py
+++ entornoEvaluacion3/evaluacionDjango/hamburgame/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Tipo(models.Model):
     id = models.IntegerField(primary_key = True)
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
     activo = models.BooleanField()
 
     def __str__(self):
