c82859f9ddeba2dfe02f22e5fecfd734a4d8f354
PabloSilvaAdones <pablo.silva.adones@gmail.com>
2021-06-09 19:27:39 -0400

    Revert "Update views.py"
    This reverts commit 0a3ccc870079787c7dbe8e309789edd97322442a.

--- entornoEvaluacion3/evaluacionDjango/hamburgame/views.py
+++ entornoEvaluacion3/evaluacionDjango/hamburgame/views.py
@@ -61,6 +61,12 @@ def eliminarProducto(request, id):
 
 
 
+
+
+
+
+
+
 def listarCategoria(request):
 # obtiene todos los datos de la tabla Categoria
     categorias = Categoria.objects.all() # select * from Categoria
@@ -132,3 +138,46 @@ def eliminarCategoria(request, id):
 
 
 
+
+
+
+
+#TIPO
+def listarTipo(request):
+    tipos = Tipo.objects.all() 
+
+    if request.method == 'POST':
+        buscar = request.POST['txtBuscar']
+        tipos = Tipo.objects.filter(nombre__contains = buscar).order_by('nombre') 
+
+    contexto = { 'categorias' : tipos  }
+    return render(request, 'listarTipo.html', contexto)
+    
+def crearTipo(request):
+    contexto = {'formulario' : TipoForm }
+  
+    if request.method == 'POST':
+        formulario = TipoForm(request.POST)
+        formulario.save()
+        contexto['mensaje'] = "Los datos fueron guardados"
+        
+    return render(request, 'crearTipo.html', contexto)
+
+# agregar solo 1 vez : from .forms import CategoriaForm 
+def modificarTipo(request, id):
+    tipo = Tipo.objects.get(id = id)
+    contexto = {'form' : TipoForm(instance=tipo) }
+
+    if request.method == 'POST':
+        formulario = TipoForm(data=request.POST, instance=tipo)
+        formulario.save()
+        contexto['mensaje'] = "Los datos fueron guardados"
+        contexto['form'] = TipoForm(instance=Tipo.objects.get(id = id))
+
+    return render(request, 'modificarTipo.html', contexto)
+
+def eliminarTipo(request, id):
+    tipo = Tipo.objects.get(id = id)
+
+    tipo.delete()
+    return redirect(to="listarTipo")
