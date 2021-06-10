36983eeb42d46b9fd49f2625539b9f5d65aea6e3
PabloSilvaAdones <pablo.silva.adones@gmail.com>
2021-06-09 22:38:06 -0400

    coloreees formulario

--- entornoEvaluacion3/evaluacionDjango/hamburgame/forms.py
+++ entornoEvaluacion3/evaluacionDjango/hamburgame/forms.py
@@ -1,22 +1,11 @@
 from django import forms
-<<<<<<< HEAD
-from .models import Producto, Categoria
-=======
 from .models import Producto, Categoria, Cliente, Pais, NivelEducacional
->>>>>>> 3b6ae540981a75c7907302d6ee3a516b84829584
 
 class ProductoForm(forms.ModelForm):
     
     class Meta:
         model = Producto
         fields= '__all__' #['id', 'codigoBarra', 'descripcion', 'precioCosto', 'precioVenta','activo', 'categoria']
-<<<<<<< HEAD
- 
-class CategoriaForm(forms.ModelForm):
-    
-    class Meta:
-        model = Categoria
-=======
  
 class CategoriaForm(forms.ModelForm):
     
@@ -40,5 +29,4 @@ class NivelEducacional(forms.ModelForm):
     
     class Meta:
         model = NivelEducacional
->>>>>>> 3b6ae540981a75c7907302d6ee3a516b84829584
         fields= '__all__' 
